563a32ede5f54ad4b118653170c72fd90fa5eed2
Shiv mishra <sm096951@gmail.com>
2023-06-14 20:00:03 +0530

    ssr

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,8 +4,8 @@ public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
-            super.cheese=true;
-            super.topping=true;
-
+        super.addExtraCheese();
+        super.addExtraToppings();
+        // your code goes here
     }
 }
